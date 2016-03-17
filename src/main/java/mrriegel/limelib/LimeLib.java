573b49586c6a10f27813ea0424b0883011bb4f1e
MrRiegel <ichhabads@gmail.com>
2016-03-17 16:28:42 +0100

    init

---
+++ src/main/java/mrriegel/limelib/LimeLib.java
@@ -0,0 +1,8 @@
+package mrriegel.limelib;
+
+public class LimeLib {
+	private LimeLib() {
+	}
+	public static final String VERSION = "1.0.0";
+
+}
---
+++ src/main/java/mrriegel/limelib/package-info.java
@@ -0,0 +1,4 @@
+@API(apiVersion = mrriegel.limelib.LimeLib.VERSION, owner = "MrRiegel", provides = "LimeLib")
+package mrriegel.limelib;
+
+import net.minecraftforge.fml.common.API;
