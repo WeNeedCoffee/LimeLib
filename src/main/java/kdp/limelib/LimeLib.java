15156b5a3f15bcf88eb168852dbfeca280120a64
gaudus <ichhabads@gmail.com>
2019-03-01 21:44:12 +0100

    setup

---
+++ src/main/java/kdp/limelib/LimeLib.java
@@ -0,0 +1,10 @@
+package kdp.limelib;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("limelib")
+public class LimeLib {
+//TODO ProtoNBT - builder & wrapper for nbttagcompound 
+	public LimeLib() {
+	}
+}
--- src/main/java/mrriegel/limelib/gui/element/ITooltip.java
+++
@@ -1,8 +0,0 @@
-package mrriegel.limelib.gui.element;
-
-public interface ITooltip {
-
-	public void drawTooltip(int mouseX, int mouseY);
-
-	// public void setTooltip(List<String> lines);
-}
